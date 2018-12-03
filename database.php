5ad984c0bc228bf0fdd9e31a66d661596020bb69
jenningscheng <45181792+jenningscheng@users.noreply.github.com>
2018-12-03 14:08:43 -0600

    Update database.php

--- database.php
+++ database.php
@@ -8,12 +8,33 @@ $sql = "CREATE DATABASE questionBank";
            // echo "Error creating database: " . mysqli_error($conn);
          }
 mysqli_select_db($conn, 'questionBank');
-// sql to create table
+
 $sql = "CREATE TABLE questions (
 answer BOOLEAN() AUTO_INCREMENT PRIMARY KEY, 
-question VARCHAR(30) NOT NULL,
+question VARCHAR(50) NOT NULL,
+);"
 
-)";
+$sql = "CREATE DATABASE studentInfo";
+         
+         if (mysqli_query($conn, $sql)) {
+           // echo "Database created successfully";
+         } else {
+           // echo "Error creating database: " . mysqli_error($conn);
+         }
+mysqli_select_db($conn, 'questionBank');
+
+$sql = "CREATE TABLE questions (
+firstName VARCHAR(10) NOT NULL,
+lastName VARCHAR(10) NOT NULL,
+score int(100)
+);"
+// sql to create table
+
+ $sql = "TRUNCATE TABLE questionBank";
+mysqli_query($conn, $sql);
+$sql = "TRUNCATE TABLE studentInfo";
+mysqli_query($conn, $sql);
+
 
 if (mysqli_query($conn, $sql)) {
    // echo "Table quizzes created successfully";
@@ -58,6 +79,69 @@ if (mysqli_query($conn, $sql)) {
 
 sql = "INSERT INTO questions (question, answer)
 VALUES ('Java is a scripting language', 'F')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('Localhost is the destination to view php files', 'T')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('We have been exposed to 3 different scripting languages so far', 'F')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('SELECT is a MySQL statement', 'T')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('Github is a popular code-collaboration site', 'T')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('Most coding applications demand high processor usage', 'F')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('There are many web resources on coding', 'T')";
+ 
+if (mysqli_query($conn, $sql)) {
+   // echo "Table quizzes created successfully";
+} else {
+   // echo "Error creating table: " . mysqli_error($conn);
+}
+
+sql = "INSERT INTO questions (question, answer)
+VALUES ('W3Schools is a well-known internet resource for web development', 'T')";
  
 if (mysqli_query($conn, $sql)) {
    // echo "Table quizzes created successfully";
